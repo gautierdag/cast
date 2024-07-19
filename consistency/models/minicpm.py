5e733aab9c0a65099cf7f816435f2c3faac347eb
Gautier Dagan <gautier.dagan@ed.ac.uk>
2024-07-19 16:01:49 +0000

    add stub for minicpm

--- consistency/models/minicpm.py
+++ consistency/models/minicpm.py
@@ -1,5 +1,3 @@
-import warnings
-
 import torch
 from transformers import (
     AutoModelForCausalLM,
@@ -22,41 +20,10 @@ class MiniCPMModel:
     def generate(
         self, instructions: str, image=None, max_new_tokens=256, start_decode: str = ""
     ) -> str:
-        prompt = f" USER: {instructions} ASSISTANT: {start_decode}"
-        if image is None:
-            inputs = self.tokenizer(prompt, return_tensors="pt").to(self.model.device)
-            # generate
-            output = self.model.generate(
-                **inputs,
-                max_new_tokens=max_new_tokens,
-                use_cache=True,
-                do_sample=False,
-            )[0]
-        else:
-            assert (
-                "<image>" in prompt
-            ), "Prompt should contain '<image>' to insert the image."
-            text_chunks = [
-                self.tokenizer(chunk).input_ids for chunk in prompt.split("<image>")
-            ]
-            input_ids = (
-                torch.tensor(
-                    text_chunks[0] + [-200] + text_chunks[1][1:], dtype=torch.long
-                )
-                .unsqueeze(0)
-                .to(self.model.device)
-            )
-            attention_mask = torch.ones_like(input_ids)
-            image_tensor = self.model.process_images([image], self.model.config).to(
-                dtype=self.model.dtype, device=self.model.device
-            )
-            # generate
-            output = self.model.generate(
-                input_ids,
-                attention_mask=attention_mask,
-                images=image_tensor,
-                max_new_tokens=max_new_tokens,
-                use_cache=True,
-                do_sample=False,
-            )[0]
-        return self.tokenizer.decode(output, skip_special_tokens=True)
+        """
+        Given an instruction and an optional image, generate a response of maximum length `max_new_tokens`.
+
+        Start decode is a string that will be appended after the instructions.
+        For instance to start the assistant response.
+        """
+        raise NotImplementedError()
